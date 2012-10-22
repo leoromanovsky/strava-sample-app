e7485b5b44b2fdc7ae81df3909699d7abb9da595
Leo Romanovsky <leo@strava.com>
2012-10-22 16:18:28 -0700

    got an access token from strava.

--- app/controllers/o_auths_controller.rb
+++ app/controllers/o_auths_controller.rb
@@ -1,6 +1,16 @@
 class OAuthsController < ApplicationController
 
   def strava
+    if params[:code]
+      client_options = {
+        site: 'http://localhost:3000',
+        parse: :json
+      }
+
+      client = OAuth2::Client.new(12, 'd9c1cd4f3e3c1e18dd07af69eb7c87299ea93ef8', client_options)
+      authorization = client.auth_code.get_token(params[:code])
+      puts "AUTH #{authorization.inspect}"
+    end
 
   end
 
