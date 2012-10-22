24ebe3460716adf4bc5bbb4863aada05e2a472b8
Leo Romanovsky <leo@strava.com>
2012-10-22 16:01:51 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '3b0dcd5e953b8efd06e2354c26cbd5ec8f07b2af3543e4eaf4b89763784bf1daf841eab8b6d2f71677fc8439577a2c2e4a819ea2459954d4d72267261f22d476'
