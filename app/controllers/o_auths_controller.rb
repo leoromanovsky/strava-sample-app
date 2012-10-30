762e091d9b7dbced7aea151979235bdaef87fad0
Leo Romanovsky <leo@strava.com>
2012-10-30 16:03:35 -0700

    moved oauth constants into config

--- app/controllers/o_auths_controller.rb
+++ app/controllers/o_auths_controller.rb
@@ -1,31 +1,22 @@
 class OAuthsController < ApplicationController
-
   def strava
     if params[:code]
-      client_options = {
-        site: 'http://localhost:3000',
-        parse: :json
-      }
+      client_options = {site: 'http://dev.strava.com', parse: :json}
 
-      client = OAuth2::Client.new(10, '3eeaa2afb05e3feb66e092f558e940769dbf65ff', client_options)
+      client = OAuth2::Client.new(Settings.strava.app_id, Settings.strava.app_secret, client_options)
       authorization = client.auth_code.get_token(params[:code])
 
       token = OAuth2::AccessToken.new(
         client,
         authorization.token,
         param_name: :access_token,
-        mode: :query
-      )
+        mode: :query)
 
-      puts "AUTH #{authorization.inspect}"
       myself = token.get('/api/v3/athlete').parsed
-      puts "MYSELF #{myself}"
 
       @athlete = myself
-
     end
   else
     @error = params[:error]
   end
-
 end
